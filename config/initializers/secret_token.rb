2402ffde2d38ff100ae593a5834495377dafee14
Patrick Jason Arthur <pa@celtic.arthurfamily.ws>
2014-01-23 15:23:50 -0500

    Add Stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitbook::Application.config.secret_key_base = 'ebb887e49a301283c6103aaa04569e264ad18d26cdbe0d1a87f40d1995f2d6aaff479cf22fed670a9ffa750f32ff2fa76b5b7aae9f83ba75d0efc086821924f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fitbook::Application.config.session_store :cookie_store, key: '_fitbook_session'
